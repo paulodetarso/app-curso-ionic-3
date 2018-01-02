ec5fa5482d01948cba25d1b02fd2f3213119ab52
Paulo de Tarso Furtado Machado <paulodetarsofm@yahoo.com.br>
2018-01-02 21:58:35 -0200

    Primeiro commit! \o/

---
+++ README.md
@@ -0,0 +1,1 @@
+"# app-curso-ionic-3" 
---
+++ src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ErrorHandler } from '@angular/core';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { FeedPage } from '../pages/feed/feed';
+import { FeedPageModule } from '../pages/feed/feed.module';
+import { IntroPage } from '../pages/intro/intro';
+import { IntroPageModule } from '../pages/intro/intro.module';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+  ],
+  imports: [
+    BrowserModule,
+    FeedPageModule,
+    IntroPageModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    FeedPage,
+    IntroPage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
